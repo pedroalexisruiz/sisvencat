4c7c6afb68184eeeda66d5cb335213136895b7dc
Administrador <Administrador@servidor.home>
2016-04-24 11:37:28 -0500

    Corregi clase Conexion

--- src/java/co/edu/ufps/Sisvencat/models/ClasesDAO/ZonaDAO.java
+++ src/java/co/edu/ufps/Sisvencat/models/ClasesDAO/ZonaDAO.java
@@ -19,7 +19,12 @@ import java.util.List;
  */
 public class ZonaDAO implements IDAOZona{
     
-    
+    private Conexion con;
+
+    public ZonaDAO() {
+        this.con = new Conexion();
+    }
+
     /**
      * insersion de una zona a la base de datos
      * @param zona objeto zona
@@ -28,23 +33,8 @@ public class ZonaDAO implements IDAOZona{
      */
     @Override
     public int insertar(Zona zona) {
-        Conexion con= new Conexion();
-        int cols = -1;
-        try{
-            con.CargaEnunciado("INSERT INTO `zona`(`Codigo_z`, `Nombre`) VALUES (?,?)");
-            con.getEnunciado().setInt(1, zona.getCodigo_z());
-            con.getEnunciado().setString(2, zona.getNombre());
-            cols=con.getEnunciado().executeUpdate();
-            
-        }
-       catch(SQLException e){
-           System.out.println(e.getMessage());
-           cols=-1;
-       }
-       finally{
-            con.close();
-        }
-       return cols;
+
+       return 0;
     }
     /**
      * modifica una zona de la base de datos
@@ -54,23 +44,8 @@ public class ZonaDAO implements IDAOZona{
      */
     @Override
     public int modificar(Zona zona) {
-        Conexion con= new Conexion();
-        int cols = -1;
-        try{
-            con.CargaEnunciado("UPDATE `zona` SET `Codigo_z`=?,`Nombre`=?");
-            con.getEnunciado().setInt(1, zona.getCodigo_z());
-            con.getEnunciado().setString(2, zona.getNombre());
-            cols=con.getEnunciado().executeUpdate();
-            
-        }
-       catch(SQLException e){
-           System.out.println(e.getMessage());
-           cols=-1;
-       }
-       finally{
-            con.close();
-        }
-       return cols;
+
+       return 0;
     }
     /**
      * Elimina una zona de la base de datos
@@ -80,21 +55,8 @@ public class ZonaDAO implements IDAOZona{
      */
     @Override
     public int eliminar(Zona zona) {
-        Conexion con= new Conexion();
-        int cols = -1;
-        try{
-            con.CargaEnunciado("DELETE FROM `zona` WHERE Codigo_z=?");
-            con.getEnunciado().setInt(1, zona.getCodigo_z());
-            cols=con.getEnunciado().executeUpdate();
-        }
-        catch(SQLException e){
-           System.out.println(e.getMessage());
-           cols=-1;
-       }
-        finally{
-            con.close();
-        }
-       return cols;
+        
+       return 0;
     }
     /**
      * lista todas las zonas que se encuentren en la base de datos
@@ -103,23 +65,6 @@ public class ZonaDAO implements IDAOZona{
     @Override
     public List<Zona> listar() {
         List<Zona> lista= new ArrayList();
-        Conexion con=new Conexion();
-        ResultSet res= con.select("*", "zona", "");
-        try{
-            while(res.next()){
-                Zona zona=new Zona();
-                zona.setCodigo_z(res.getInt(1));
-                zona.setNombre(res.getString(2));
-                lista.add(zona);
-            }
-            
-        }
-        catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
-        finally{
-            con.close();
-        }
            
         return lista;
     }
@@ -131,29 +76,9 @@ public class ZonaDAO implements IDAOZona{
     @Override
     public List<Zona> listar(String where) {
         List<Zona> lista= new ArrayList();
-        Conexion con=new Conexion();
-        ResultSet res= con.select("*", "zona",where);
-        try{
-            while(res.next()){
-                Zona zona=new Zona();
-                zona.setCodigo_z(res.getInt(1));
-                zona.setNombre(res.getString(2));
-                lista.add(zona);
-            }
-            
-        }
-        catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
-        finally{
-            con.close();
-        }
+
            
         return lista;
     }
 
-   
-    
-
-    
 }
--- src/java/co/edu/ufps/Sisvencat/models/util/Conexion.java
+++ src/java/co/edu/ufps/Sisvencat/models/util/Conexion.java
@@ -2,11 +2,8 @@ package co.edu.ufps.Sisvencat.models.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,19 +14,17 @@ import java.util.logging.Logger;
 public class Conexion {
 
     private Statement consulta;
-    private ResultSet resultado;
-    private PreparedStatement enunciado;
     private Connection conexion;
     private final String db_url = "jdbc:mysql://sandbox2.ufps.edu.co/ufps_98";
     private final String db_driver = "com.mysql.jdbc.Driver";
     private final String db_username = "ufps_98";
-    private final String db_password = "ufps_98";
+    private final String db_password = "ufps_uy";
     /**
-     * además de crear el objeto sde genera la conexion y se crea la consulta
+     * además de crear el objeto se genera la conexion y se crea la consulta
      */
     public Conexion() {
         try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Class.forName(db_driver).newInstance();
             conexion = DriverManager.getConnection(db_url, db_username, db_password);
             consulta = conexion.createStatement();
         } catch (SQLException e) {
@@ -46,13 +41,6 @@ public class Conexion {
      * cierra la conexion a la base de datos.
      */
     public void close() {
-        if (resultado != null) {
-            try {
-                resultado.close();
-            } catch (Exception e) {
-            }
-            resultado = null;
-        }
 
         if (consulta != null) {
             try {
@@ -60,14 +48,6 @@ public class Conexion {
             } catch (Exception e) {
             }
             consulta = null;
-        }
-
-        if (enunciado != null) {
-            try {
-                enunciado.close();
-            } catch (Exception e) {
-            }
-            enunciado = null;
         }
 
         if (conexion != null) {
@@ -79,40 +59,6 @@ public class Conexion {
             conexion = null;
         }
 
-    }
-    /**
-     * carga un enunciado de tipo preparedStatement
-     * @param sentencia es la sentencia sql a utilizar, puede ser parametrizada
-     * y además se deber usar para insersiones, actualizaciones y eliminaciones. 
-     */
-    public void CargaEnunciado(String sentencia) {
-        try {
-            enunciado = conexion.prepareStatement(sentencia);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-    /**
-     * obtiene el enunciado preparedStatemet que se habia cargado antes.
-     * @return un enunciado PreparedStetement
-     */
-    public PreparedStatement getEnunciado() {
-        return enunciado;
-    }
-
-    public ResultSet select(String atributos, String tabla, String where) {
-
-        try {
-            if (!where.isEmpty()) {
-                where = "WHERE" + where;
-            }
-
-            resultado = consulta.executeQuery("SELECT " + atributos + "FROM " + tabla + "" + where + ";");
-
-        } catch (SQLException ex) {
-            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return resultado;
     }
     /**
      * se obtine la consulta ya cargada en el constructor
