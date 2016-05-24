02fa2ace9390d4cd399c5948f2b66dbdd13d4221
Pedro <Administrador@servidor.home>
2016-05-24 21:26:01 +0400

    PedidoDAO
    Pedidos

--- src/java/co/edu/ufps/Sisvencat/models/ClasesDAO/PedidoDAO.java
+++ src/java/co/edu/ufps/Sisvencat/models/ClasesDAO/PedidoDAO.java
@@ -25,7 +25,7 @@ import java.util.List;
  *
  * @author estudiante
  */
-public class PedidoDAO implements Serializable, IDAOPedido{
+public class PedidoDAO implements Serializable, IDAOPedido {
 
     private Conexion con;
     public static final DateFormat formater = new SimpleDateFormat("yyyy-MM-dd");
@@ -33,7 +33,7 @@ public class PedidoDAO implements Serializable, IDAOPedido{
     public PedidoDAO() throws SQLException {
         con = new Conexion();
     }
-    
+
     @Override
     public boolean insertar(Pedido pe) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -51,30 +51,30 @@ public class PedidoDAO implements Serializable, IDAOPedido{
 
     @Override
     public List<Pedido> listarPorCampaña(Campaña cam) throws SQLException, ParseException {
-        
+
         String consulta = "SELECT pedido.*,persona.*,premio.* FROM pedido INNER JOIN premio ON "
                 + "pedido.Premio_Codigo_prem=premio.Codigo_prem WHERE "
                 + "pedido.Campana_Codigo_cam=?";
         List<Pedido> pedidos = new ArrayList();
-        
-        if(con==null){
+
+        if (con == null) {
             con = new Conexion();
         }
-        
+
         PreparedStatement state = con.getConexion().prepareStatement(consulta);
         state.setLong(1, cam.getCodigo_cam());
         ResultSet rs = state.executeQuery();
-        
+
         Pedido p = null;
         Premio pre = null;
-        
-        while(rs.next()){
-            pre = new Premio(rs.getInt("Codigo_prem"),rs.getString("Nombre_Premio"),rs.getString("Descripcion"),rs.getInt("Punto_requerido"),rs.getInt("CantDisponible"),null);
+
+        while (rs.next()) {
+            pre = new Premio(rs.getInt("Codigo_prem"), rs.getString("Nombre_Premio"), rs.getString("Descripcion"), rs.getInt("Punto_requerido"), rs.getInt("CantDisponible"), null);
             Calendar fechapedido = Calendar.getInstance();
             fechapedido.setTime(formater.parse(rs.getString("Fecha_pedido")));
-            
-            p = new Pedido(rs.getInt("Codigo_pedido"),pre, rs.getString("Vendedor_Persona_Cedula"),rs.getInt("Valor_total"),fechapedido);
-            
+
+            p = new Pedido(rs.getInt("Codigo_pedido"), pre, rs.getString("Vendedor_Persona_Cedula"), rs.getInt("Valor_total"), fechapedido);
+
             pedidos.add(p);
         }
         this.closeConn();
@@ -83,36 +83,71 @@ public class PedidoDAO implements Serializable, IDAOPedido{
 
     @Override
     public Pedido getPedido(Pedido pe) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    @Override
-    public Pedido getPedidoDelVendedor(String cedula, int codigo_cam) throws SQLException, ParseException{
-        
-        String consulta = "SELECT * FROM pedido WHERE Vendedor_Persona_Cedula=? AND Campana_Codigo_cam=?";
-        if(con==null){
-            con = new Conexion();
-        }
-        
-        PreparedStatement state = con.getConexion().prepareStatement(consulta);
-        state.setString(1, cedula);
-        state.setInt(2, codigo_cam);
-        ResultSet rs = state.executeQuery();
-        
+        String consulta = "SELECT * FROM pedido WHERE ";
         Pedido pedido = null;
+        PreparedStatement state = null;
+        ResultSet rs = null;
         
-        while(rs.next()){
-            Calendar fechapedido = Calendar.getInstance();
-            fechapedido.setTime(formater.parse(rs.getString("Fecha_pedido")));
-            pedido = new Pedido(rs.getInt("Codigo_pedido"),null,cedula,rs.getInt("Valor_Total"),fechapedido);
+        try{
+            if(con==null){
+                con = new Conexion();
+            }
+            state = con.getConexion().prepareStatement(consulta);
+            state.setLong(1, pe.getCodigo_pedido());
+            rs = state.executeQuery();
+            
+            while(rs.next()){
+                pedido = new Pedido();
+            }
+        }catch(SQLException e){
+            throw e;
         }
-        
         return pedido;
     }
+
     @Override
-    public void closeConn() throws SQLException{
+    public Pedido getPedidoDelVendedor(String cedula, int codigo_cam) throws SQLException, ParseException {
+
+        String consulta = "SELECT * FROM pedido WHERE Vendedor_Persona_Cedula=? AND Campana_Codigo_cam=?";
+        PreparedStatement state = null;
+        ResultSet rs = null;
+        Pedido pedido = null;
+        
+        try {
+            if (con == null) {
+                con = new Conexion();
+            }
+
+            state = con.getConexion().prepareStatement(consulta);
+            state.setString(1, cedula);
+            state.setInt(2, codigo_cam);
+            rs = state.executeQuery();
+
+            while (rs.next()) {
+                Calendar fechapedido = Calendar.getInstance();
+                fechapedido.setTime(formater.parse(rs.getString("Fecha_pedido")));
+                pedido = new Pedido(rs.getInt("Codigo_pedido"), null, cedula, rs.getInt("Valor_Total"), fechapedido);
+            }
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if(state!=null){
+                state.close();
+            }
+            if(rs!=null){
+                rs.close();
+            }
+            if(con!=null){
+                this.closeConn();
+            }
+        }
+        return pedido;
+    }
+
+    @Override
+    public void closeConn() throws SQLException {
         con.close();
         con = null;
     }
-    
+
 }
